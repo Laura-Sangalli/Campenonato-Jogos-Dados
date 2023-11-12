6157cf000c768a9ce55594686f81367d4320b790
Laura Sangalli <lauraasangalli@gmail.com>
2023-11-12 19:34:01 -0300

    so organizei os nomes das funções nos arquivos

--- Campeonato.java
+++ Campeonato.java
@@ -1,3 +1,27 @@
 public class Campeonato {
-    
+    private Jogador jogadores[] = new Jogador[10];
+
+    public void incluirJogador(){
+
+    } 
+
+    public void removerJogador(){
+
+    }
+
+    public void iniciarCampeonato(){
+
+    }
+
+    public void mostrarCartela(){
+
+    }
+
+    public void gravarEmArquivo(){
+
+    }
+
+    public void lerDoArquivo(){
+        
+    }
 }
--- Jogador.java
+++ Jogador.java
@@ -1,3 +1,25 @@
 public class Jogador {
-    
+    private String nome;
+    private double saldo;
+    private JogoDados[] jogos = new JogoDados[10];
+
+    public Jogador(String nome, double saldo){
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public void jogarDados(){
+
+    }
+
+    public String toString(){
+        String str = "";
+
+        return str;
+    }
+
+    public void mostraJogadasExecutadas(){
+        
+    }
+
 }
