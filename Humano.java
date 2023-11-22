69e734fd26e4e09187a6fd323105d32dfff8726f
Brunonunesdasilva <brun0.silva6358@gmail.com>
2023-11-22 14:14:50 -0300

    att4

--- Humano.java
+++ Humano.java
@@ -17,27 +17,14 @@ public class Humano extends Jogador implements JogarComoHumano{
     public void jogarDados(){
         int jogoEscolhido = Jogador.escolherJogo();
 
-<<<<<<< HEAD
-        while(jogoEscolhido != 1 || jogoEscolhido != 2){
-            if(jogoEscolhido == 1){ //jogoGeneral
-                JogarComoHumano.escolherJogadas(jogoG);
-            }
-            else if(jogoEscolhido == 2){
-                jogoA.rolarDados();
-                jogoA.resultado();
-            }
-            else{
-                System.out.println("Escolha um jogo válido");
-            }
-=======
         if(jogoEscolhido == 1){ //jogoGeneral
-            
             JogarComoHumano.escolherJogadas(jogoG);
         }
         else if(jogoEscolhido == 2){
             jogoA.rolarDados();
-            jogoA.resultado();
->>>>>>> 3ccc87848ce8e08b47816a7efc69542446e4cd15
+            jogoA.resultado();            }
+        else{
+            System.out.println("Escolha um jogo válido");
         }
     }
 
--- Jogador.java
+++ Jogador.java
@@ -7,20 +7,19 @@ public abstract class Jogador {
 
     public Jogador(String nome, double saldo){
         this.nome = nome;
-        this.saldo = saldo;
-     
+        this.saldo = saldo;     
     }
 
     public static int escolherJogo(){
         int value=0;
         Scanner scanner = new Scanner(System.in);
             
-        while(value != 1 || value != 2){   
+        while(value != 1 && value != 2){   
             System.out.println("Informe o número correspondente a qual jogo voce deseja apostar: \n1 - Jogo Azar; \n2 - Jogo General.");
             value = scanner.nextInt();
-            scanner.close();
+            //scanner.close();
 
-            if(value != 1 || value != 2){
+            if(value != 1 && value != 2){
                 System.out.println("O valor informado eh invalido!");
             }
         }
